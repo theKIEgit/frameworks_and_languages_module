7f054fbfec4000ce8a86a409cff6b92966f245ab
Allan Callaghan <calaldees@gmail.com>
2022-02-16 12:25:40 +0000

    Error reporting for express

--- examples/server/express_test_2/app.js
+++ examples/server/express_test_2/app.js
@@ -15,6 +15,12 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// https://expressjs.com/en/guide/error-handling.html
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 // Docker container exit handler
 // https://github.com/nodejs/node/issues/4182
 process.on('SIGINT', function() {
