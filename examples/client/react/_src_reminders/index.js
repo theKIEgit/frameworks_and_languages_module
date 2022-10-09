06737caab8b405998645eb3e2af2df39d478561a
calaldees <calaldees@gmail.com>
2022-10-09 16:12:51 +0000

    Tweaked react example

---
+++ examples/client/react/_src_reminders/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import RootComponent from './myapp';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RootComponent />);
