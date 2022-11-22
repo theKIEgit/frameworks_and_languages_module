e71aef1ca53c43939013bf7e5d3331861d4711d0
Kieran R <kieranrutter2002@outlook.com>
2022-11-22 15:29:55 +0000

    Added cards

--- server/items.js
+++ server/items.js
@@ -4,7 +4,7 @@ var items = {
         "user_id": "user1234",
         "keywords": ["hammer", "nails", "tools"],
         "description": "A hammer and nails set",
-        "image": "https://www.placekitten.com/200/100",
+        "image": "https://placekitten.com/200/300",
         "lat": 1.10,
         "lon": 1.20,
         "date_from": "2022-11-22T08:22:39.067408",
