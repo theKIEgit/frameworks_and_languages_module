4344d91ba1bf11f7a1115201582baee0c51231a7
Kieran R <kieranrutter2002@outlook.com>
2022-11-22 11:32:46 +0000

    All test pass

--- server/items.js
+++ server/items.js
@@ -1,15 +1,5 @@
 var items = {
-    1: {
-        "id": 1,
-        "user_id": "user1234",
-        "keywords": ["hammer", "nails", "tools"],
-        "description": "A hammer and nails set",
-        "image": "https://www.placekitten.com/200/100",
-        "lat": 1.10,
-        "lon": 1.20,
-        "date_from": "2022-11-22T08:22:39.067408",
-        "date_to": "2022-11-22T08:22:39.067408"
-    },
+
   }
   module.exports = items;
   
